91436ec6a04f50dc5f66de30dd4604d63fe0cf28
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-23 16:49:16 +0300

    completed

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,48 @@
 def create_an_empty_array
+    Array.new
   
 end
 
 def create_an_array
+    array = [1,2,"array","4"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+    #array << element
+    array.push(element)
+    array 
   
 end
 
 def add_element_to_start_of_array(array, element)
+   array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop 
+    puts array
 end
 
 def remove_element_from_start_of_array(array)
+   array.shift
+  
   
 end
 
 def retrieve_element_from_index(array, index_number)
+    array[index_number]
   
 end
 
 def retrieve_first_element_from_array(array)
+   puts  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+    array[-1]
+    puts array
   
 end
